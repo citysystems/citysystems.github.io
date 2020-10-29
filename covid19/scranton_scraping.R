f5c525ddad221c1001738d9e8d29ed91d416773d
derekouyang <30915437+derekouyang@users.noreply.github.com>
2020-10-28 23:56:32 -0700

    update

--- covid19/scranton_scraping.R
+++ covid19/scranton_scraping.R
@@ -2,11 +2,23 @@
 # for use in Github Actions workflow
 
 library(esri2sf)
+library(tidyverse)
+library(sf)
 
-data <- esri2sf("https://services1.arcgis.com/Nifc7wlHaBPig3Q3/ArcGIS/rest/services/ZIP_Code_PA_COVID/FeatureServer/0")
+data <- esri2sf("https://services1.arcgis.com/Nifc7wlHaBPig3Q3/ArcGIS/rest/services/ZIP_Code_PA_COVID/FeatureServer/0") %>% 
+  st_set_geometry(NULL) %>% 
+  select(
+    ZIP_CODE,
+    Positive,
+    Negative,
+    Probable
+  ) %>% 
+  mutate(
+    date = Sys.Date()
+  )
 
-write.csv(data, paste0("covid19/pa-zip-", Sys.Date(), ".csv"))
+write_csv(data, paste0("covid19/pa-zip-", Sys.Date(), ".csv"))
 
 scrape_time_df <- data.frame(scrape_last_time_ran = Sys.time())
 
-write.csv(scrape_time_df, "covid19/scranton_scrape_last_time_ran.csv")
+write_csv(scrape_time_df, "covid19/scranton_scrape_last_time_ran.csv")
