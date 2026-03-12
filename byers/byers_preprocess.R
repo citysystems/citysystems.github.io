02653525758e0f28e0442918b999297764b01167
Derek Ouyang <derekouyang@gmail.com>
2026-03-11 22:25:02 -0700

    Add Byers scraping workflow and preprocess R
    Add a scheduled GitHub Actions workflow (byers_scraping.yml) that installs R, required system packages and R libraries, runs byers/byers_preprocess.R, commits and pushes updated data, and emails on failure. Include the R preprocessing script (byers/byers_preprocess.R) which reads Google Sheets ranges, tidies project/faculty/area/country data and saves them to byers/byers_data.rda. This enables automated daily data refresh and storage of the resulting .rda file.

---
+++ byers/byers_preprocess.R
@@ -0,0 +1,63 @@
+library(dplyr)
+library(tidyr)
+library(googlesheets4)
+gs4_deauth()
+
+data <-
+  range_speedread("1B3UzqR0yOsUZVVjjhgvMuxfIHiDPd8atgchxwcZP2-Y",range="Projects") %>% 
+  mutate(index = row_number())
+
+data_projects <- data %>% 
+  select(
+    index,
+    `Project Name`,
+    `Link for additional information`,
+    `Hospital/Facility name`,
+    `Partner Agencies (separate with commas)`
+  )
+
+data_areas <-
+  range_speedread("1B3UzqR0yOsUZVVjjhgvMuxfIHiDPd8atgchxwcZP2-Y",range="Areas", col_names = FALSE)
+
+data_faculty <-
+  range_speedread("1B3UzqR0yOsUZVVjjhgvMuxfIHiDPd8atgchxwcZP2-Y",range="Faculty")
+
+data_countries <-
+  range_speedread("1B3UzqR0yOsUZVVjjhgvMuxfIHiDPd8atgchxwcZP2-Y",range="Countries", col_names = FALSE)
+
+data_tidy <- data %>% 
+  select(
+    index,
+    starts_with(c(
+      "Faculty",
+      "Clinical/academic focus area",
+      "Country"
+    ))
+  ) %>% 
+  pivot_longer(
+    starts_with("Faculty"),
+    values_to = "Faculty"
+  ) %>% 
+  select(-name) %>% 
+  filter(!is.na(Faculty)) %>% 
+  pivot_longer(
+    starts_with("Clinical/academic focus area"),
+    values_to = "Area"
+  ) %>% 
+  select(-name) %>% 
+  filter(!is.na(Area)) %>% 
+  pivot_longer(
+    starts_with("Country"),
+    values_to = "Country"
+  ) %>% 
+  select(-name) %>% 
+  filter(!is.na(Country))
+
+save(
+  data_projects,
+  data_areas,
+  data_faculty,
+  data_countries,
+  data_tidy,
+  file = "byers/byers_data.rda"
+)
